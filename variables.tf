d9aaabdf3f7c13074bf9d2b48d4251740d6502a8
Subhani <46126222+SubhaniShaik960@users.noreply.github.com>
2022-03-28 16:08:56 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "ami_id" {
+   default     = "ami-0e472ba40eb589f49"
+ }
+variable "instance_type" {
+   default     = "t2.micro"
+ }
+variable "key_name" {
+   default     = "Jenkins_subhani"
+ } 
+variable "tag_name" {
+   default     = "Modules.Include"
+ }
